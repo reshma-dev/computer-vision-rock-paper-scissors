e2aefab0946e5633b3844c11ad9d6f11bfd50466
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-10-25 11:14:32 +0100

    Creates a function to pick a random choice

---
+++ manual_rps.py
@@ -0,0 +1,15 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+print("Choices: ",  choices)
+def get_computer_choice():
+    return random.choice(choices)
+
+def get_user_choice():
+    while True:
+        choice = input("Pick your choice from rock / paper / scissors: ")
+        if choice.lower() in choices:
+            return choice.lower()
+
+print("Computer choice: ", get_computer_choice())
+print("User choice: ", get_user_choice())
