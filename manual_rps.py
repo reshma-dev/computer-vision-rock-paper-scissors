e8dd266c9881c72b1abf87e5c93e9c6ce86e3000
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-10-25 12:26:00 +0100

    Adds a function to decide who won

--- manual_rps.py
+++ manual_rps.py
@@ -15,6 +15,31 @@ def get_user_choice():
         if choice.lower() in choices:
             return choice.lower()
 
+# Function to choose a winner
+def get_winner(computer_choice, user_choice):
+    
+    user_msg = "User"
+    computer_msg = "Computer"
+    winner = None
+
+    if computer_choice == user_choice:
+        print("It's a tie!")
+        return winner
+    
+    if computer_choice == 'rock' and user_choice == 'paper':
+        winner = user_msg
+    elif computer_choice == 'paper' and user_choice == 'scissors':
+        winner = user_msg
+    elif computer_choice == 'scissors' and user_choice == 'rock':
+        winner = user_msg
+    else:
+        winner = computer_msg
+
+    return winner
+
 # Test functions
-print("User choice: ", get_user_choice())
-print("Computer choice: ", get_computer_choice())
+user_choice = get_user_choice()
+computer_choice = get_computer_choice()
+
+print("Computer choice: ", computer_choice, ", User choice: ", user_choice)
+print("Winner: ", get_winner(computer_choice, user_choice))
