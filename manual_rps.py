a10c46c07dc391bbbdfa87c4fad5218ef15afbce
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-10-25 11:19:47 +0100

    Adds comments

--- manual_rps.py
+++ manual_rps.py
@@ -1,10 +1,14 @@
 import random
 
+# Available valid choices
 choices = ["rock", "paper", "scissors"]
 print("Choices: ",  choices)
+
+# Function to randomly pick a choice 
 def get_computer_choice():
     return random.choice(choices)
 
+# Function to ask user to pick an option until they enter a valid choice
 def get_user_choice():
     while True:
         choice = input("Pick your choice from rock / paper / scissors: ")
